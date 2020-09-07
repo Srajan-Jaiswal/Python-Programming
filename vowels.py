3bc106178e93b843eba399bfe4e24476a70faed6
Srajan Jaiswal <68801705+Srajan-Jaiswal@users.noreply.github.com>
2020-09-08 00:44:47 +0530

    Create vowels.py

---
+++ vowels.py
@@ -0,0 +1,7 @@
+m = input("Enter Character : ")
+
+if(m == 'a' or m == 'e' or m == 'i' or m == 'o' or m == 'u' or m == 'A'
+       or m == 'E' or m == 'I' or m == 'O' or m == 'U'):
+    print(m, "is a Vowel")
+else:
+    print(m, "is a Consonant")
